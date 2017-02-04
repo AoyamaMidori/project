57f26b5b8c8e2079172ec3cf685e34f768ea09bf
manB <manB@email.local>
2017-02-04 23:52:27 +0900

    server uses random available port

--- hello.go
+++ hello.go
@@ -3,14 +3,34 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"net"
+	"log"
 )
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "hello")
 }
 
+func listenOnPortAvailable() (net.Listener, string) {
+	ln, err := net.Listen("tcp", "localhost:")
+	if err != nil {
+		log.Fatal(err)
+	}
+	_, port, err := net.SplitHostPort(ln.Addr().String())
+	if err != nil {
+		log.Fatal(err)
+	}
+	return ln, port
+}
+
+func makeServer() {
+	ln, port := listenOnPortAvailable()
+	fmt.Println("server is launched on port", port)
+	http.HandleFunc("/hello", helloHandler)
+	http.Serve(ln, nil)
+}
+
 func main() {
 	fmt.Println("hello")
-	http.HandleFunc("/hello", helloHandler)
-	http.ListenAndServe(":50000", nil)
+	makeServer()
 }
