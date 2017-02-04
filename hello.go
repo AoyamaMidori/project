c2593c53041217e4fa9442611753e1061b1aa450
manA <manA@email.local>
2017-02-04 23:29:17 +0900

    hello server

--- hello.go
+++ hello.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
+func helloHandler(w http.ResponseWriter, req *http.Request) {
+	fmt.Fprintln(w, "hello")
+}
 
 func main() {
 	fmt.Println("hello")
+	http.HandleFunc("/hello", helloHandler)
+	http.ListenAndServe(":50000", nil)
 }
