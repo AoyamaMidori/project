eff6cbb471c60c10c34254b9f5aa904672f256e3
manB <manB@email.local>
2017-02-05 17:22:12 +0900

    Add handler for version

--- hello.go
+++ hello.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"runtime"
 )
 
 func byeHandler(w http.ResponseWriter, req *http.Request) {
@@ -13,6 +14,21 @@ func byeHandler(w http.ResponseWriter, req *http.Request) {
 
 func helloHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "hello")
+}
+
+func versionHandler(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintln(w, "<ol>")
+	defer fmt.Fprintln(w, "</ol>")
+	for k, v := range map[string]interface{}{
+		"OS":       runtime.GOOS,
+		"ARCH":     runtime.GOARCH,
+		"Ver":      runtime.Version(),
+		"Compiler": runtime.Compiler,
+	} {
+		fmt.Fprintf(w, "<li>%s = %v</li>", k, v)
+	}
 }
 
 func listenOnPortAvailable() (net.Listener, string) {
@@ -31,6 +47,7 @@ func makeServer() {
 	ln, port := listenOnPortAvailable()
 	fmt.Println("server is launched on port", port)
 	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/version", versionHandler)
 	http.Serve(ln, nil)
 }
 
